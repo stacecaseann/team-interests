6a1ee089292396c5daa306a62e107c3a4640a3d5
Nefi Zaldana <nefizaldana@gmail.com>
2025-10-10 21:42:22 -0600

    nefi collections/routes/controller/validation/user

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const userRoutes = require('./users');
 const movieRoutes = require('./movies');
 const speakerRoutes = require('./conferenceSpeakers');
 const favoriteBooksRoutes = require('./favoritebooks');
+const programmingLanguagesRoutes = require('./programmingLanguages');
 const passport = require('passport');
 
 // Swagger setup
@@ -20,6 +21,7 @@ router.use('/recipes', recipeRoutes);
 router.use('/movies', movieRoutes);
 router.use('/speakers', speakerRoutes);
 router.use('/favoritebooks', favoriteBooksRoutes);
+router.use('/programmingLaunguages', programmingLanguagesRoutes);
 router.get('/login', passport.authenticate('github'));
 
 router.get('/logout', function (req, res, next) {
---
+++ routes/programmingLanguages.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+const { isAuthenticated } = require('../middleware/authentication');
+
+const programmingLanguagesController = require('../controllers/programmingLanguagesController');
+const {
+  validateObjectId,
+  validateProgrammingLanguageData,
+  handleValidationErrors,
+} = require('../middleware/validation');
+
+router.get('/', programmingLanguagesController.getLanguages);
+
+router.get('/:id', validateObjectId, programmingLanguagesController.getLanguageById);
+
+router.post(
+  '/',
+  isAuthenticated,
+  validateProgrammingLanguageData,
+  handleValidationErrors,
+  programmingLanguagesController.createLanguage,
+);
+
+router.put(
+  '/:id',
+  isAuthenticated,
+  validateObjectId,
+  validateProgrammingLanguageData,
+  handleValidationErrors,
+  programmingLanguagesController.updateLanguage,
+);
+
+router.delete(
+  '/:id',
+  isAuthenticated,
+  validateObjectId,
+  programmingLanguagesController.deleteLanguage,
+);
+
+module.exports = router;
